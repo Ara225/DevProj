a01486467b5627d16da9592fd6789c8d1724ed0a
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-11-08 23:39:44 +0000

    infra: Finalize DB structure

--- Infrastructure/src/ProjectManagerInfra/ProjectManagerInfraStack.cs
+++ Infrastructure/src/ProjectManagerInfra/ProjectManagerInfraStack.cs
@@ -8,18 +8,20 @@ namespace ProjectManagerInfra
         internal ProjectManagerInfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             Table ProjectIdeasDBProd = new Table(this, "ProjectIdeasDBProd", new TableProps
-                                {
-                                    PartitionKey = new Attribute { 
-                                        Name = "Type", 
-                                        Type = AttributeType.STRING 
-                                    },
-                                    SortKey = new Attribute { 
-                                        Name = "Id", 
-                                        Type = AttributeType.STRING 
-                                    },
-                                    BillingMode = BillingMode.PAY_PER_REQUEST,
-                                    TableName = "ProjectIdeasDBProd"
-                                });
+            {
+                PartitionKey = new Attribute
+                {
+                    Name = "Type",
+                    Type = AttributeType.NUMBER
+                },
+                SortKey = new Attribute
+                {
+                    Name = "Id",
+                    Type = AttributeType.STRING
+                },
+                BillingMode = BillingMode.PAY_PER_REQUEST,
+                TableName = "ProjectIdeasDBProd"
+            });
         }
     }
 }
