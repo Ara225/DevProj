590448297edb5f0d98fe96e0ee989c056666900f
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-11-24 00:24:02 +0000

    Add more tests

--- DevProjUnitTests/UserStoreUnitTests.cs
+++ DevProjUnitTests/UserStoreUnitTests.cs
@@ -2,6 +2,7 @@ using DynamoDBUserStore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace DevProjUnitTests
     {
         private InMemoryUserStore _store;
         private DynamoDBUser _user;
-
+        
         [TestInitialize]
         public void Initialize()
         {
@@ -23,10 +24,135 @@ namespace DevProjUnitTests
         }
 
         [TestMethod]
-        public async Task TestGetUser()
+        public async Task TestLoginFunctions()
+        {
+            await _store.AddLoginAsync(_user, new UserLoginInfo("TestProvider", "RandomKey", "TestProviderDN"), new CancellationToken());
+            DynamoDBUser user = await _store.FindByLoginAsync("TestProvider", "RandomKey", new CancellationToken());
+            Assert.AreEqual(user.Id, _user.Id);
+            Assert.AreEqual(user.LoginProviderKeys.Count, 1);
+            Assert.AreEqual(user.LoginProviderKeys[0], "RandomKey");
+            IList<UserLoginInfo> LoginInfo = await _store.GetLoginsAsync(_user, new CancellationToken());
+            Assert.AreEqual(LoginInfo[0].ProviderKey, "RandomKey");
+            await _store.RemoveLoginAsync(_user, "TestProvider", "RandomKey", new CancellationToken());
+        }
+
+        [TestMethod]
+        public async Task TestFindByEmail()
+        {
+            DynamoDBUser user = await _store.FindByEmailAsync(_user.NormalizedEmail, new CancellationToken());
+            Assert.AreEqual(user.Id, _user.Id);
+        }
+
+        [TestMethod]
+        public async Task TestFindById()
         {
             DynamoDBUser user = await _store.FindByIdAsync(_user.Id, new CancellationToken());
             Assert.AreEqual(user.Id, _user.Id);
+        }
+
+        [TestMethod]
+        public async Task TestFindByName()
+        {
+            DynamoDBUser user = await _store.FindByNameAsync(_user.NormalizedUserName, new CancellationToken());
+            Assert.AreEqual(user.Id, _user.Id);
+        }
+
+        [TestMethod]
+        public async Task TestGetEmail()
+        {
+            string Email = await _store.GetEmailAsync(_user, new CancellationToken());
+            Assert.AreEqual(_user.Email, Email);
+        }
+
+        [TestMethod]
+        public async Task TestGetEmailConfirmed()
+        {
+            bool EmailConfirmed = await _store.GetEmailConfirmedAsync(_user, new CancellationToken());
+            Assert.AreEqual(_user.EmailConfirmed, EmailConfirmed);
+        }
+
+        [TestMethod]
+        public async Task TestGetNormalizedEmail()
+        {
+            string NormalizedEmail = await _store.GetNormalizedEmailAsync(_user, new CancellationToken());
+            Assert.AreEqual(_user.NormalizedEmail, NormalizedEmail);
+        }
+
+        [TestMethod]
+        public async Task TestGetNormalizedUserName()
+        {
+            string NormalizedUserName = await _store.GetNormalizedUserNameAsync(_user, new CancellationToken());
+            Assert.AreEqual(_user.NormalizedUserName, NormalizedUserName);
+        }
+
+        [TestMethod]
+        public async Task TestGetPasswordHash()
+        {
+            string PasswordHash = await _store.GetPasswordHashAsync(_user, new CancellationToken());
+            Assert.AreEqual(_user.PasswordHash, PasswordHash);
+        }
+
+        [TestMethod]
+        public async Task TestGetUserId()
+        {
+            string Id = await _store.GetUserIdAsync(_user, new CancellationToken());
+            Assert.AreEqual(_user.Id, Id);
+        }
+
+        [TestMethod]
+        public async Task TestGetUserName()
+        {
+            string UserName = await _store.GetUserNameAsync(_user, new CancellationToken());
+            Assert.AreEqual(_user.UserName, UserName);
+        }
+
+        [TestMethod]
+        public async Task TestHasPassword()
+        {
+            bool HasPassword = await _store.HasPasswordAsync(_user, new CancellationToken());
+            Assert.AreEqual(HasPassword, true);
+        }
+
+        [TestMethod]
+        public async Task TestSetEmail()
+        {
+            await _store.SetEmailAsync(_user, "user@example.com", new CancellationToken());
+        }
+
+        [TestMethod]
+        public async Task TestSetEmailConfirmed()
+        {
+            await _store.SetEmailConfirmedAsync(_user, true, new CancellationToken());
+        }
+
+        [TestMethod]
+        public async Task TestSetNormalizedEmail()
+        {
+            await _store.SetNormalizedEmailAsync(_user, "USER@EXAMPLE.COM", new CancellationToken());
+        }
+
+        [TestMethod]
+        public async Task TestSetNormalizedUserName()
+        {
+            await _store.SetNormalizedUserNameAsync(_user, "TESTUSER", new CancellationToken());
+        }
+
+        [TestMethod]
+        public async Task TestSetPasswordHash()
+        {
+            await _store.SetPasswordHashAsync(_user, "TESTUSER", new CancellationToken());
+        }
+
+        [TestMethod]
+        public async Task TestSetUserName()
+        {
+            await _store.SetUserNameAsync(_user, "TestUser2", new CancellationToken());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            IdentityResult Result = _store.DeleteAsync(_user, new CancellationToken()).Result;
         }
     }
 }
