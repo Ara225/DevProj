562df91d1317f9883d3ee6434f08f7a5a2fc81ae
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-11-30 19:13:31 +0000

    Add base project data and view model

---
+++ DevProjWebApp/Models/ProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DevProjWebApp.Models
+{
+    public class ProjectViewModel
+    {
+        public string Name { get; set; }
+
+        public List<string> LinkedRepositoriesURLs { get; set; }
+        
+        public string Description { get; set; }
+
+        public bool isPrivate { get; set; }
+    }
+}
---
+++ DynamoDBDataAccess/ProjectDataModel.cs
@@ -0,0 +1,55 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamoDBDataAccess
+{
+    [DynamoDBTable("DevProjProjectsTable")]
+    public class ProjectDataModel
+    {
+        public ProjectDataModel(string ProjectName, string ProjectDescription, string ProjectOwnerId, bool ProjectIsPrivate)
+        {
+            if (ProjectName == null || ProjectDescription == null || ProjectOwnerId == null || isPrivate == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Name = ProjectName;
+            NormalizedName = ProjectName.ToUpper();
+            Description = ProjectDescription;
+            OwnerId = ProjectOwnerId;
+            isPrivate = ProjectIsPrivate;
+            CreatedOn = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        }
+
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string NormalizedName { get; set; }
+
+        /// <summary>
+        /// Id of user who owns the project
+        /// </summary>
+        public string OwnerId { get; set; }
+
+        public List<string> LinkedRepositoriesURLs { get; set; }
+        
+        public string CreatedOn { get; set; }
+
+        public string Description { get; set; }
+
+        public bool isPrivate { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
