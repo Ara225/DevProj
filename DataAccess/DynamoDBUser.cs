5f6e46da1de59ba6acb8e0cf1484071105dde500
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-12-07 18:34:46 +0000

    Scaffold identity UI

--- DataAccess/DynamoDBUser.cs
+++ DataAccess/DynamoDBUser.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace DataAccess
 {
     [DynamoDBTable("DevProjUsersTable")]
-    public class DynamoDBUser
+    public class DynamoDBUser : IdentityUser
     {
         //
         // Summary:
@@ -46,91 +46,10 @@ namespace DataAccess
 
         //
         // Summary:
-        //     Gets or sets the date and time, in UTC, when any user lockout ends.
-        //
-        // Remarks:
-        //     A value in the past means the user is not locked out.
-        public string? LockoutEnd { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets a flag indicating if two factor authentication is enabled for this
-        //     user.
-        //
-        // Value:
-        //     True if 2fa is enabled, otherwise false.
-        [PersonalData]
-        public bool TwoFactorEnabled { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets a flag indicating if a user has confirmed their telephone address.
-        //
-        // Value:
-        //     True if the telephone number has been confirmed, otherwise false.
-        [PersonalData]
-        public bool PhoneNumberConfirmed { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets a telephone number for the user.
-        [ProtectedPersonalData]
-        public string PhoneNumber { get; set; }
-
-        //
-        // Summary:
-        //     A random value that must change whenever a user is persisted to the store
-        public string ConcurrencyStamp { get; set; }
-
-        //
-        // Summary:
-        //     A random value that must change whenever a users credentials change (password
-        //     changed, login removed)
-        public string SecurityStamp { get; set; }
-        //
-        // Summary:
-        //     Gets or sets a salted and hashed representation of the password for this user.
-        public string PasswordHash { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets a flag indicating if a user has confirmed their email address.
-        //
-        // Value:
-        //     True if the email address has been confirmed, otherwise false.
-        [PersonalData]
-        public bool EmailConfirmed { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets the normalized email address for this user.
-        [ProtectedPersonalData]
-        public string NormalizedEmail { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets the email address for this user.
-        [ProtectedPersonalData]
-        public string Email { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets the normalized user name for this user.
-        [ProtectedPersonalData]
-        public string NormalizedUserName { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets the user name for this user.
-        [ProtectedPersonalData]
-        public string UserName { get; set; }
-
-        //
-        // Summary:
         //     Gets or sets the primary key for this user.
         [PersonalData]
         [DynamoDBHashKey]
-        public string Id { get; set; }
+        override public string Id { get; set; }
 
         //
         // Summary:
@@ -142,19 +61,6 @@ namespace DataAccess
         // Summary:
         //     Gets or sets the date the user was created
         public string CreatedOn { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets a flag indicating if the user could be locked out.
-        //
-        // Value:
-        //     True if the user could be locked out, otherwise false.
-        public bool LockoutEnabled { get; set; }
-
-        //
-        // Summary:
-        //     Gets or sets the number of failed login attempts for the current user.
-        public int AccessFailedCount { get; set; }
 
         //
         // Summary:
