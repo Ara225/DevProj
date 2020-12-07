cd4873e23146d89143ef97d7b90a1684105c4cb4
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-12-07 23:15:16 +0000

    Restyle login page

--- DevProjWebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ DevProjWebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace DevProjWebApp.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember Me")]
             public bool RememberMe { get; set; }
         }
 
